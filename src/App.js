e7d41b1223edaf357ddcc8014b4be98f5d6c6834
Paul Nizet <polonizet@gmail.com>
2022-09-05 19:00:13 +0200

    add tailwind + creditCard component in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import IdCard from './components/IdCard';
 import Greetings from './components/Greetings';
 import Random from './components/Random';
 import BoxColor from './components/BoxColor';
+import CreditCard from './components/CreditCard';
 
 function App() {
   return (
@@ -19,6 +19,16 @@ function App() {
       <Greetings lang="fr">Ludwig</Greetings>
       <Random min={1} max={1090} />
       <BoxColor r={0} g={0} b={255} />
+      <CreditCard
+        type="Mastercard"
+        number="0123456789018845"
+        expirationMonth={3}
+        expirationYear={2021}
+        bank="BNP"
+        owner="Maxence Bouret"
+        bgColor="#11aa99"
+        color="white" 
+      />
     </div>
   );
 }
--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import visa from '../assets/images/visa.png'
+import mastercard from '../assets/images/mastercard.png'
+
+
 
 function CreditCard({type, number, expirationMonth, expirationYear, bank, owner, bgColor, color}) {
   return (
-    <div>CreditCard</div>
+    <div className="container flex flex-col mx-auto rounded-lg shadow border p-8 m-10 w-[30rem]" style={{background: `${bgColor}`, color: `${color}`}}>
+        <div className="flex flex-col">
+
+          <div className="flex ">
+            <img className="aligns-end w-16" src={type === 'Visa' ? visa : mastercard} alt="visa" />
+          </div>
+          <div className="flex justify-center">{number}</div>
+          <div className="flex justify-start">Expires {expirationMonth}/{expirationYear} {bank}</div>
+          <div className="flex justify-start">{owner}</div>
+        </div>
+    </div>
   )
 }
 
