ab920bc66ef12e3f2e3a0444317203861553e08c
Paul Nizet <polonizet@gmail.com>
2022-09-05 17:42:50 +0200

    add box color and credit card

--- src/components/BoxColor.js
+++ src/components/BoxColor.js
@@ -3,7 +3,7 @@ import React from 'react'
 function BoxColor({r, g, b}) {
 // Create a BoxColor component that displays a rectangle with a background color based on props.
     return (
-        <div style={{background: `rgb(${r}, ${g}, ${b})`}}>Wazzaaaa</div>
+        <div style={{background: `rgb(${r}, ${g}, ${b})`}}>rgb({r}, {g}, {b})</div>
     )
 }
 
---
+++ src/components/CreditCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreditCard({type, number, expirationMonth, expirationYear, bank, owner, bgColor, color}) {
+  return (
+    <div>CreditCard</div>
+  )
+}
+
+export default CreditCard
