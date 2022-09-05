7db225ecd415de2c7c79f94bbc3217b27af8ad61
Paul Nizet <polonizet@gmail.com>
2022-09-05 17:00:37 +0200

    create Random component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import IdCard from './components/IdCard';
 import Greetings from './components/Greetings';
+import Random from './components/Random';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         picture="https://randomuser.me/api/portraits/men/44.jpg"
       />
       <Greetings lang="fr">Ludwig</Greetings>
+      <Random min={1} max={1090} />
     </div>
   );
 }
