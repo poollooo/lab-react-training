34b47b644c8755f437b82b716c8ce1ea4f3f044b
Paul Nizet <polonizet@gmail.com>
2022-09-06 12:45:11 +0200

    fix credit car styling

--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -9,8 +9,8 @@ function CreditCard({type, number, expirationMonth, expirationYear, bank, owner,
     <div className="container flex flex-col mx-auto rounded-lg shadow border p-8 m-10 w-[30rem]" style={{background: `${bgColor}`, color: `${color}`}}>
         <div className="flex flex-col">
 
-          <div className="flex ">
-            <img className="aligns-end w-16" src={type === 'Visa' ? visa : mastercard} alt="visa" />
+          <div className="flex flex-row justify-end">
+            <img className="w-16" src={type === 'Visa' ? visa : mastercard} alt="visa" />
           </div>
           <div className="flex justify-center">{number}</div>
           <div className="flex justify-start">Expires {expirationMonth}/{expirationYear} {bank}</div>
